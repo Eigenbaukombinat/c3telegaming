e41740c82e8764bd7ee7ba88182d03cd6a469933
Markus-Be <markus_be@mailbox.org>
2024-12-08 20:56:43 +0100

    file required absolute path

--- hotline/hotline.tcl
+++ hotline/hotline.tcl
@@ -1,22 +1,24 @@
 #!/usr/bin/tclsh
 package require ygi
 
+set sounds "/usr/local/share/yate/sounds"
+
 ::ygi::start_ivr
 ::ygi::set_dtmf_notify
 
 ::ygi::idle_timeout
 
-::ygi::play_wait "yintro"
+::ygi::play_wait "$sounds/yintro"
 ::ygi::sleep 500
 
 while { true } {
-  set digit [::ygi::play_getdigit file "pocmenu/123"]
+  set digit [::ygi::play_getdigit file "$sounds/pocmenu/123"]
   if { $digit == "1" } {
-       ::ygi::play_getdigit file "pocmenu/warteschleife" stopdigits { 3 }
+       ::ygi::play_getdigit file "$sounds/pocmenu/warteschleife" stopdigits { 3 }
   }
   
   if { $digit == "2" } {
-       ::ygi::play_getdigit file "pocmenu/clan_chi_telekom" stopdigits { 3 }
+       ::ygi::play_getdigit file "$sounds/pocmenu/clan_chi_telekom" stopdigits { 3 }
   }
 
 }
